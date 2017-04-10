39023702c701af50f17d760cbe2473b016f86a06
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-10 14:29:15 -0700

    Removed extra test, added eslint ignore for console.log

--- power-cli.js
+++ power-cli.js
@@ -3,5 +3,6 @@ const powerOf = require('./power');
 
 const base = parseInt(process.argv[2]);
 const exponent = parseInt(process.argv[3]);
-
+    
+/*eslint no-console: off*/
 console.log(powerOf(base, exponent));
--- test/power.test.js
+++ test/power.test.js
@@ -8,9 +8,4 @@ describe('power of', () => {
     assert.equal(result, 27);
   });
 
-  it('returns a value when no numbers are entered', () => {
-    const stringResult = powerOf(3, 3);
-    assert.equal(stringResult, 'Please enter two numbers');
-  });
-
 });
