dc2fe52f2734cf0fe4bc87f1e976405307fe69b8
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-10 14:51:06 -0700

    Initial set up complete, basic mocha test passed

---
+++ test/avg.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const avgOf = require('../avg.js');
+
+describe('test average', () => {
+  it('returns the average of two numbers', () => {
+    //
+    const result = 3.2;
+    assert.equal(result, 3.2);
+  });
+});
