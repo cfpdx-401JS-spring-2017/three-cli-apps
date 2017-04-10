d898062bbf776af4658f19e00861d7564079a600
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-10 15:48:10 -0700

    Basic mocha test passed

---
+++ day-greet.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function calculateGreeting() {
+
+}
+
+// 00:00 - 11:59 morning
+// 12:00 - 16:59 afternoon
+// 17:00 - 23:59 night
---
+++ test/day-greet.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('day greeting', () => {
+  it('returns greeting with name and time of day', () => {
+    //
+    return 0;
+  });
+});
