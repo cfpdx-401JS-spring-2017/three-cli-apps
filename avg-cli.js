0cecb33e384728d5482d6af3e9104bdf92b16c69
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-10 15:36:40 -0700

    Full CLI test passing

--- avg-cli.js
+++ avg-cli.js
@@ -2,6 +2,9 @@
 
 const avgOf = require('./avg');
 const inputArray = process.argv.slice(2);
+const intArray = inputArray.map( function(i) {
+  return parseInt(i);
+});
 
 /*eslint no-console: off*/
-console.log(avgOf(inputArray));
+console.log(avgOf(intArray));
--- avg.js
+++ avg.js
@@ -1,13 +1,10 @@
 'use strict';
 
 function calculateAvgOf(cliArray) {
-  console.log(cliArray);
   var sum = cliArray.reduce( function(acc, curr) {
     acc += curr;
     return acc;
   }, 0);
-  console.log('sum', sum);
-  console.log('Length of array', cliArray.length);
   let avg = sum / cliArray.length;
   return avg;
 }
