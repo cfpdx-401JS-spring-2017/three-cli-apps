ba102e7a1ddc3a36e77e8ec1701ce12a1db4fa77
Jane Smith <example@email.com>
2017-04-10 14:09:27 -0700

    changed file names to power

--- test/power.test.js
+++ test/power.test.js
@@ -1,7 +1,7 @@
 //describe is what we're testing
 const assert = require('assert');
 //../takes you up a level....requiring the power-of.js file
-const powerOf = require('../power-of');
+const powerOf = require('../power');
 
 describe('power of', () => {
     //it is each individual test case
