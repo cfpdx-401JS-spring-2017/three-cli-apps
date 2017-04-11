1f7def02152d823e24ee2cc2a35e21d58173c103
Ivy Patton <lilaivy@gmail.com>
2017-04-10 22:47:49 -0700

    added comments to test

--- test/day-greet.test.js
+++ test/day-greet.test.js
@@ -2,18 +2,20 @@ const assert = require('assert');
 const dayGreet = require('../day-greet');
 describe('dayGreet', () => {
     it('provides approriate morning greeting based on time of day' , () => {
-        //assert.equal(actual, expected)
-        const greeting = dayGreet('11:05', 'Ivy');
+        const greeting = dayGreet('10', 'Ivy');
         assert.equal(greeting,'Good morning, Ivy!');
     });
     it('provides approriate afternoon greeting based on time of day' , () => {
-        //assert.equal(actual, expected)
-        const greeting = dayGreet('13', 'Ivy');
+        const greeting = dayGreet('16', 'Ivy');
         assert.equal(greeting,'Good afternoon, Ivy!');
     });
      it('provides approriate evening greeting based on time of day' , () => {
-        //assert.equal(actual, expected)
-        const greeting = dayGreet('17:15', 'Ivy');
+        const greeting = dayGreet('20', 'Ivy');
         assert.equal(greeting,'Good evening, Ivy!');
     });
 });
+
+//why does it not always return actual(green) expected (red)?
+//what's up with hours 2-9?  Returns good evening?
+//assert.equal(actual, expected)
+     
