d96332792860810ec76efe39a8027417a5ad47a0
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-10 17:00:13 -0700

    MVP of lab complete

--- day-greet.js
+++ day-greet.js
@@ -1,17 +1,14 @@
 'use strict';
 
-function calculateGreeting(name, time) {
+function calculateGreeting(name, time = 'Day') {
   const intTime = parseInt(time.split(':').join(''));
-  console.log('Converted time', intTime);
   if (0 <= intTime && intTime <= 1159) {
     time = 'Morning';
   } else if (1200 <= intTime && intTime <= 1659) {
     time = 'Afternoon';
   } else if (1700 <= intTime && intTime <= 2359) {
     time = 'Evening';
-  } else {
-    time = 'Day'; //edge case if time is not entered
-  };
+  }
   return `Good ${time}, ${name}.`;
 }
 
