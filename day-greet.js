81c41717ce4f7a7fc52132af646fa25adc377740
Ivy Patton <lilaivy@gmail.com>
2017-04-18 22:42:46 -0700

    Added parseInt to number strings

--- day-greet.js
+++ day-greet.js
@@ -2,13 +2,13 @@
 //1. write a basic function that returns zero and 'module.exports = basic function that you wrote'
 
 function dayGreet(hour, name) {
-  if (hour >= '0:00' && hour <= '11:59'){
+  if (hour >= parseInt('00:00') && hour <= parseInt('11:59')){
     return 'Good morning, ' + name + '!';
 
-  } else if (hour >= '12:00' && hour <= '16:59'){
+  } else if (hour >= parseInt('12:00') && hour <= parseInt('16:59')){
     return 'Good afternoon, ' + name + '!';
 
-  } else if (hour >= '17:00' && hour <= '23:59'){
+  } else if (hour >= parseInt('17:00') && hour <= parseInt('23:59')){
     return 'Good evening, ' + name + '!';
   };
   return dayGreet;
--- test/day-greet.test.js
+++ test/day-greet.test.js
@@ -4,15 +4,15 @@ const assert = require('assert');
 const dayGreet = require('../day-greet');
 describe('dayGreet', () => {
     it('provides approriate morning greeting based on time of day' , () => {
-        const greeting = dayGreet('10', 'Ivy');
+        const greeting = dayGreet(8, 'Ivy');
         assert.equal(greeting,'Good morning, Ivy!');
     });
     it('provides approriate afternoon greeting based on time of day' , () => {
-        const greeting = dayGreet('16', 'Ivy');
+        const greeting = dayGreet(16, 'Ivy');
         assert.equal(greeting,'Good afternoon, Ivy!');
     });
      it('provides approriate evening greeting based on time of day' , () => {
-        const greeting = dayGreet('20', 'Ivy');
+        const greeting = dayGreet(22, 'Ivy');
         assert.equal(greeting,'Good evening, Ivy!');
     });
 });
